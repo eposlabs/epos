2519e817c036bb37f4034d7cbb844466a58408e5
imkost <k.zemtsovsky@gmail.com>
2025-08-19 15:24:39 +0300

    Experimenting with Vite

---
+++ packages/temp/vite-project/src/main.fg.tsx
@@ -0,0 +1,4 @@
+import App from './App'
+
+// console.log(BUNDLE)
+console.log('fg', App)
---
+++ packages/temp/vite-project/vite.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { postBuildPlugin } from './post-build-plugin'
+
+export default defineConfig({
+  plugins: [
+    react({}),
+    postBuildPlugin({
+      // TODO:
+      // define: {},
+      // banner: { js: [], css: [] },
+      // footer: {},
+      // bundles: {
+      //   fg: {
+      //     define,banner,footer
+      //   }
+      // }
+      // + any esbuild options
+
+      define: {
+        default: {
+          DATA: 'default-data',
+        },
+        fg: {
+          BUNDLE: 'fg-bundle',
+        },
+        bg: {
+          BUNDLE: 'bg-bundle',
+        },
+      },
+    }),
+  ],
+  // resolve: {
+  //   alias: {
+  //     react: 'preact/compat',
+  //     'react-dom': 'preact/compat',
+  //   },
+  // },
+  //  esbuild: {
+  //   jsxFactory: 'h',
+  //   jsxFragment: 'Fragment',
+  // },
+  build: {
+    watch: {},
+    minify: false,
+    sourcemap: true,
+    outDir: 'a',
+    rollupOptions: {
+      input: {
+        fg: 'src/main.fg.tsx',
+        bg: 'src/main.bg.tsx',
+      },
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+})
