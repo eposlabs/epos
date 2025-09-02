89910881a75dfaf2cfe9dd61d4f0c8930c474c06
imkost <k.zemtsovsky@gmail.com>
2025-09-02 17:16:49 +0300

    Remove dev hub auto reload

--- projects/ext/src/app/pkgs/pkgs.ex.ts
+++ projects/ext/src/app/pkgs/pkgs.ex.ts
@@ -4,23 +4,10 @@ export class Pkgs extends $ex.Unit {
   map: { [name: string]: $ex.Pkg } = {}
   watcher = new $exOsVw.PkgsWatcher(this)
 
-  async init() {
-    await this.initDevHubAutoReload()
-  }
-
   create(decl: PkgDeclaration) {
     if (this.map[decl.name]) throw this.never
     const pkg = new $ex.Pkg(this, decl)
     this.map[decl.name] = pkg
     return pkg
-  }
-
-  private async initDevHubAutoReload() {
-    if (!this.$.env.is.exTabHub) return
-    await this.watcher.start(delta => {
-      const names = Object.keys(this.map)
-      const changed = delta.updated.some(name => names.includes(name))
-      if (changed) location.reload()
-    })
   }
 }
