d50f2f9625e2e9c3919ab82b745984440aeec90c
imkost <k.zemtsovsky@gmail.com>
2025-11-28 14:33:13 +0200

    Remove KitLogger

--- apps/ext/src/app/kit/kit.ex.ts
+++ apps/ext/src/app/kit/kit.ex.ts
@@ -1,5 +1,4 @@
 export class Kit extends ex.Unit {
   browser = new ex.KitBrowser(this)
   fetcher = new ex.KitFetcher(this)
-  logger = new ex.KitLogger(this)
 }
--- apps/ext/src/app/kit/kit.sw.ts
+++ apps/ext/src/app/kit/kit.sw.ts
@@ -1,5 +1,4 @@
 export class Kit extends sw.Unit {
   browser = new sw.KitBrowser(this)
   fetcher = new sw.KitFetcher(this)
-  logger = new sw.KitLogger(this)
 }
