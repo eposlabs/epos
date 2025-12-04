1fb1968c5fa8e1514959b8993127b694f50e361b
imkost <k.zemtsovsky@gmail.com>
2025-12-04 15:49:54 +0200

    Refactor app.sw

--- apps/ext/src/app/app.sw.ts
+++ apps/ext/src/app/app.sw.ts
@@ -17,11 +17,11 @@ export class App extends sw.Unit {
     self.$ = this
     await this.net.init()
     await this.projects.init()
-    await this.initOffscreen()
-    await this.initContentScript()
-    await this.reloadDevKitTabs()
-    this.initGlobalMethods()
     this.logDevHelp()
+    this.initGlobalMethods()
+    await this.setupContentScript()
+    await this.createOffscreen()
+    await this.reloadDevKitTabs()
     await this.dev.init()
   }
 
@@ -32,17 +32,31 @@ export class App extends sw.Unit {
     this.$.browser.tabs.create({ url, active: true })
   }
 
-  private async initOffscreen() {
-    const exists = await this.$.browser.offscreen.hasDocument()
-    if (exists) await this.$.browser.offscreen.closeDocument()
-    await this.$.browser.offscreen.createDocument({
-      url: this.$.env.url.offscreen,
-      reasons: ['BLOBS'],
-      justification: 'URL.createObjectURL',
-    })
+  private logDevHelp() {
+    const hasDevProject = this.projects.list.some(project => project.dev)
+    if (!hasDevProject) return
+
+    const version = chrome.runtime.getManifest().version
+    console.log(
+      [
+        `%cᛃ epos ${version} https://epos.dev/docs/api`,
+        '%c\n',
+        `%cTo inspect <background> process, open offscreen.html from the extension details page`,
+      ].join(''),
+      'border-left: 2px solid #d7eb00; padding-left: 8px; padding-top: 1px; padding-bottom: 4px;',
+      '',
+      'border-left: 2px solid #d7eb00; padding-left: 8px; padding-bottom: 1px; margin-bottom: 8px; color: gray;',
+    )
   }
 
-  private async initContentScript() {
+  private initGlobalMethods() {
+    self.add = (name: string, dev = false) => this.projects.installer.install(name, dev)
+    self.remove = (name: string) => this.projects.installer.remove(name)
+    self.eject = (name: string, dev = false) => this.projects.map[name].exporter.export(dev)
+    self.install = self.add
+  }
+
+  private async setupContentScript() {
     // Unregister previous content script
     const contentScripts = await this.$.browser.scripting.getRegisteredContentScripts()
     await this.$.browser.scripting.unregisterContentScripts({ ids: contentScripts.map(cs => cs.id) })
@@ -66,32 +80,21 @@ export class App extends sw.Unit {
     ])
   }
 
+  private async createOffscreen() {
+    const exists = await this.$.browser.offscreen.hasDocument()
+    if (exists) await this.$.browser.offscreen.closeDocument()
+    await this.$.browser.offscreen.createDocument({
+      url: this.$.env.url.offscreen,
+      reasons: ['BLOBS'],
+      justification: 'URL.createObjectURL',
+    })
+  }
+
   private async reloadDevKitTabs() {
     const tabs = await this.browser.tabs.query({ url: 'https://epos.dev/@devkit*' })
     for (const tab of tabs) {
       if (!tab.id) continue
       this.browser.tabs.reload(tab.id)
     }
-  }
-
-  private initGlobalMethods() {
-    self.add = (name: string, dev = false) => this.projects.installer.install(name, dev)
-    self.remove = (name: string) => this.projects.installer.remove(name)
-    self.eject = (name: string, dev = false) => this.projects.map[name].exporter.export(dev)
-    self.install = self.add
-  }
-
-  private logDevHelp() {
-    const hasDevProject = this.projects.list.some(project => project.dev)
-    if (!hasDevProject) return
-
-    const version = chrome.runtime.getManifest().version
-
-    console.log(`ᛃ epos is running, v${version} | https://epos.dev/docs/api`)
-    console.log(
-      '%cTo inspect <background> process, open offscreen.html from the extension details page',
-      'color: gray;',
-    )
-    console.log('')
   }
 }
