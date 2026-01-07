371c408cbe2112e1f3e29721bb3da2db63fa9bb9
imkost <k.zemtsovsky@gmail.com>
2026-01-07 05:14:10 +0200

    Better reloader

--- apps/ext/src/core/reloader.sw.ts
+++ apps/ext/src/core/reloader.sw.ts
@@ -1,18 +1,52 @@
 if (DEV) {
-  const ws = new WebSocket(`ws://localhost:${import.meta.env.REBUNDLE_PORT}`)
+  let ws: WebSocket | null = null
+  void reconnect(0)
 
-  ws.addEventListener('message', async e => {
-    // Don't reload if only `vw` bundle is changed
-    const bundles = JSON.parse(e.data) as string[]
-    if (bundles.length === 1 && bundles[0] === 'vw') return
+  async function reconnect(delay = 1000) {
+    if (ws) ws.close()
+    await wait(delay)
+    if (!(await ping())) return reconnect()
+    if (ws) return onReconnect()
+    ws = new WebSocket(`ws://localhost:${import.meta.env.REBUNDLE_PORT}`)
+    ws.addEventListener('close', () => onDisconnect())
+    ws.addEventListener('error', () => onDisconnect())
+    ws.addEventListener('message', () => reload())
+  }
 
-    // Close all side panels. Chrome crashes if extension is reloaded while at least one side panel is open.
+  async function onReconnect() {
+    console.log('🟢 Dev Server connected')
+    await reload(500)
+  }
+
+  async function onDisconnect() {
+    console.log('🔴 Dev Server disconnected')
+    reconnect()
+  }
+
+  async function ping() {
     try {
-      const tabs = await $.browser.tabs.query({})
-      for (const tab of tabs) await $.tools.medium.closeSidePanel(tab.id)
+      await fetch(`http://localhost:${import.meta.env.REBUNDLE_PORT}`)
+      return true
+    } catch {
+      return false
+    }
+  }
+
+  async function reload(delay = 0) {
+    // Close all side panels.
+    // Chrome crashes if extension is reloaded while at least one side panel is open.
+    try {
+      const tabs = await chrome.tabs.query({})
+      for (const tab of tabs) await chrome.sidePanel.setOptions({ tabId: tab.id, enabled: false })
     } catch {}
 
     // Reload extension
+    await wait(delay)
     chrome.runtime.reload()
-  })
+  }
+
+  async function wait(ms: number) {
+    if (ms <= 0) return
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
 }
