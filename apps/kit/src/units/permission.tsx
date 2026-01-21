24453b038c37426a88f5aed1e86d251450850214
imkost <k.zemtsovsky@gmail.com>
2026-01-21 23:11:02 +0200

    Minor changes

--- apps/kit/src/units/permission.tsx
+++ apps/kit/src/units/permission.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@ui/lib/utils.js'
+
 export class Permission extends gl.Unit {
   name: chrome.runtime.ManifestPermission
 
@@ -7,40 +9,53 @@ export class Permission extends gl.Unit {
   }
 
   async play() {
+    // contextMenus
     if (this.name === 'contextMenus') {
       await epos.browser.contextMenus.removeAll()
       epos.browser.contextMenus.create({
-        id: 'remove-all-context-menus',
         title: 'Remove all context menus',
         contexts: ['all'],
       })
-      epos.browser.contextMenus.onClicked.addListener(async info => {
-        if (info.menuItemId === 'remove-all-context-menus') {
-          await epos.browser.contextMenus.removeAll()
-        }
+      epos.browser.contextMenus.onClicked.addListener(async () => {
+        await epos.browser.contextMenus.removeAll()
       })
-    } else if (this.name === 'downloads') {
+    }
+
+    // downloads
+    else if (this.name === 'downloads') {
       const blob = new Blob(['test-file'], { type: 'text/plain' })
       const url = URL.createObjectURL(blob)
       await epos.browser.downloads.download({ url, filename: 'test-file.txt' })
       URL.revokeObjectURL(url)
-    } else if (this.name === 'notifications') {
+    }
+
+    // notifications
+    else if (this.name === 'notifications') {
       await epos.browser.notifications.create({
         type: 'basic',
         iconUrl: '/icon.png',
         title: 'Test Notification',
         message: 'This is a test notification',
       })
-    } else if (this.name === 'cookies') {
+    }
+
+    // cookies
+    else if (this.name === 'cookies') {
       await epos.browser.cookies.set({
         url: 'https://epos.dev',
         name: 'epos:checked',
         value: 'true',
         expirationDate: Date.now() / 1000 + 60 * 60 * 24 * 30,
       })
-    } else if (this.name === 'storage') {
+    }
+
+    // storage
+    else if (this.name === 'storage') {
       await epos.browser.storage.local.set({ 'epos:checked': true })
-    } else if (this.name === 'browsingData') {
+    }
+
+    // browsingData
+    else if (this.name === 'browsingData') {
       await epos.browser.browsingData.remove({ origins: ['https://epos.dev'] }, { serviceWorkers: true })
     }
   }
@@ -50,7 +65,9 @@ export class Permission extends gl.Unit {
       <div className="flex flex-col">
         <button
           onClick={this.play}
-          className="flex cursor-pointer gap-2 rounded-sm bg-gray-200 px-3 py-1.5 text-left hover:brightness-95 dark:bg-black"
+          className={cn(
+            'flex cursor-pointer gap-2 rounded-sm bg-gray-200 px-3 py-1.5 text-left hover:brightness-95 dark:bg-black',
+          )}
         >
           <div>{this.name}</div>
         </button>
