c32df53233eeb7caecdf39ccf6b44a457916ce61
imkost <k.zemtsovsky@gmail.com>
2026-01-22 12:14:24 +0200

    Add release command

---
+++ scripts/release.ts
@@ -0,0 +1,27 @@
+// npm run release -- [patch|minor|major=patch] [workspace1] [workspace2] ...
+// ---------------------------------------------------------------------------
+
+import { execSync } from 'node:child_process'
+
+void (() => {
+  const { version, targets } = parseArgs()
+  const workspacesArg = targets.map(target => `-w ${target}`).join(' ')
+  run(`npm version ${version} --workspaces-update=false ${workspacesArg}`)
+  run(`syncpack fix-mismatches`)
+  run(`npm run build ${workspacesArg}`)
+  run(`npm publish ${workspacesArg}`)
+  run(`npm install`)
+})()
+
+function parseArgs() {
+  const args = process.argv.slice(2)
+  if (args[0] === 'patch' || args[0] === 'minor' || args[0] === 'major') {
+    return { version: args[0], targets: args.slice(1) }
+  } else {
+    return { version: 'patch', targets: args }
+  }
+}
+
+function run(command: string) {
+  execSync(command, { stdio: 'inherit', cwd: process.cwd(), env: process.env })
+}
