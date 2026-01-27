9907fc0b94484dad151bb7001f6648455fc9b9a0
imkost <k.zemtsovsky@gmail.com>
2026-01-27 11:31:28 +0200

    Minor epos/vite refactoring

--- packages/epos/src/epos-vite.ts
+++ packages/epos/src/epos-vite.ts
@@ -1,31 +1,23 @@
 import { resolve } from 'node:path'
 import type { Plugin } from 'vite'
 
-export class EposVite {
-  private libs: Record<string, string> = {
-    'react': resolve(import.meta.dirname, './epos-vite-react.js'),
-    'react/jsx-runtime': resolve(import.meta.dirname, './epos-vite-react-jsx-runtime.js'),
-    'react-dom': resolve(import.meta.dirname, './epos-vite-react-dom.js'),
-    'react-dom/client': resolve(import.meta.dirname, './epos-vite-react-dom-client.js'),
-    'mobx': resolve(import.meta.dirname, './epos-vite-mobx.js'),
-    'mobx-react-lite': resolve(import.meta.dirname, './epos-vite-mobx-react-lite.js'),
-  }
-
-  get plugin(): Plugin {
-    return {
-      name: 'epos',
-      enforce: 'pre',
-      resolveId: this.onResolveId,
-    }
-  }
-
-  private onResolveId = (source: string) => {
-    return this.libs[source] ?? null
-  }
+const libs: Record<string, string> = {
+  'react': resolve(import.meta.dirname, './epos-vite-react.js'),
+  'react/jsx-runtime': resolve(import.meta.dirname, './epos-vite-react-jsx-runtime.js'),
+  'react-dom': resolve(import.meta.dirname, './epos-vite-react-dom.js'),
+  'react-dom/client': resolve(import.meta.dirname, './epos-vite-react-dom-client.js'),
+  'mobx': resolve(import.meta.dirname, './epos-vite-mobx.js'),
+  'mobx-react-lite': resolve(import.meta.dirname, './epos-vite-mobx-react-lite.js'),
 }
 
-export function epos() {
-  return new EposVite().plugin
+export function epos(): Plugin {
+  return {
+    name: 'epos',
+    enforce: 'pre',
+    resolveId(source) {
+      return libs[source] ?? null
+    },
+  }
 }
 
 export default epos
