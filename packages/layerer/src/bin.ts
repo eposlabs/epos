0bb5fb01e946adf39129d377ca9b06f82b718793
imkost <k.zemtsovsky@gmail.com>
2026-04-14 21:46:33 +0300

    Use noEmit: true by default

--- packages/layerer/src/bin.ts
+++ packages/layerer/src/bin.ts
@@ -15,21 +15,11 @@ const argv = minimist(process.argv.slice(2), {
 })
 
 const paths = argv._
-const input = paths.slice(0, -1)
+const inputs = paths.slice(0, -1)
 const output = paths.at(-1)
 
-if (input.length === 0) {
-  console.error('Input directory not provided')
-  process.exit(1)
-}
-
-if (!output) {
-  console.error('Output directory not provided')
-  process.exit(1)
-}
-
-await layerer({
-  input: input,
+void layerer({
+  input: inputs,
   output: output,
   watch: argv.watch,
   expose: argv.expose,
